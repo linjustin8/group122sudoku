a010ca7082eb8d1c316b6cba661de5bafaad3af6
luiswtheds <cabreraluis@ufl.edu>
2023-04-19 19:16:54 -0400

    Made the basic framework for a GUIII BABY!!!!

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,11 @@
-print('test')
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000,1000))
+
+while True:
+    screen.fill("purple")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
