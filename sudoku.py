9a41db796f53258cfb06d322806312cfc54465ea
Sanjeev <sanjeev.kamath@ufl.edu>
2023-04-30 16:51:36 -0400

    Final

--- sudoku.py
+++ sudoku.py
@@ -276,10 +276,11 @@ def main():
                         if sudoku.board[pos[0]][pos[1]] == 0:
                             sudoku.sketch(sketch)
                     if event.key == pygame.K_DELETE or event.key == pygame.K_BACKSPACE:
-                        sudoku.board[pos[0]][pos[1]] = 0
-                        sudoku.clear()
-                        sudoku.draw()
-                        sudoku.update_board()
+                        if sudoku.original[pos[0]][pos[1]] == 0:
+                            sudoku.board[pos[0]][pos[1]] = 0
+                            sudoku.clear()
+                            sudoku.draw()
+                            sudoku.update_board()
                     if event.key == pygame.K_RETURN :
                         if sudoku.original[pos[0]][pos[1]] == 0:
                             sudoku.place_number(sketch)
